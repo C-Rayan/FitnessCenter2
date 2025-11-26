22da631ca416a44606bfe1bd297011ea4002e3f5
C-Rayan <chrayan246@gmail.com>
2025-11-25 20:40:49 -0500

    Latest Fix

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args){
+        Member alien = new Member();
+        alien.setName("Jamal");
+        alien.setEmail("Jamal@gmail.com");
+
+
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(org.example.Member.class);
+        config.configure("hibernate.cfg.xml");
+
+        SessionFactory sf = config.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(alien);
+
+        transaction.commit();
+    }
+}
---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private int aid;
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
